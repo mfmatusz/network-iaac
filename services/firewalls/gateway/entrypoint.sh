bfbde4fb9c355db4d20124f60394b27f76458a3a
mfmatusz <maciejmatuszewski030303@gmail.com>
2025-01-14 13:55:10 +0100

    small changes related to VPN, it does not work as thought earlier

--- services/firewalls/gateway/entrypoint.sh
+++ services/firewalls/gateway/entrypoint.sh
@@ -2,6 +2,7 @@
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables-restore < /etc/iptables/rules
+ipsec start
 
 ip route del default && ip route add default via 10.1.0.2
 ip route add 192.168.0.0/24 via 10.1.0.2
